561eea12c2de237c821a29937bd0cde911d8d53d
Mielein <marie.trojan@uni-weimar.de>
2020-07-08 14:56:24 +0200

    all implemented bugs need to be fixed

--- source/ass4.cpp
+++ source/ass4.cpp
@@ -4,9 +4,9 @@
 #include <iostream>
 #include <cmath>
 #include <iterator>
+#include <algorithm>
 
-std::vector<point> point_vec;
-/* //method to compare points of x coordinate
+//method to compare points of x coordinate
 int comparePoints_x(point const& a, point const& b){
     return a.x - b.x;
 }
@@ -15,99 +15,79 @@ int comparePoints_y(point const& a, point const& b){
     return a.y - b.y;
 }
 float distance(point const& a, point const& b){
-    return sqrt(pow(comparePoints_x(a,b))+ pow(comparePoints_y(a,b)));
+    return sqrt(pow(comparePoints_x(a,b),2)+ pow(comparePoints_y(a,b),2));
 }
-float min(float x, float y){
-    return (x<y)? x : y;
-}
-float bruteForce(std::vector<point> point_vev, int n){
-    float min;
-    for(int i = 0; i < n; ++i){
-        for(int j = i+1; j<n;++j){
-            if(distance(point_vec[i],point_vec[j])<min){
-                min = distance(point_vec[i],point_vec[j]);
-            }
-        }
-    }
-    return min;
-}
-float closest_y(std::vector<point> vec_y, int size, float dist){
-    float min = dist;
-    qsort(vec_y,size,sizeof(point),comparePoints_y());
-    for(int i = 0;i<size;++i){
-        for(distance(vec_y[i],vec_y[j])<min){
-            min = distance(vec_y[i], vec_y[j]);
-        }
-    }
-    return min;
-}
-float closest_x(std::vector<point> vec_x, int size){
-    if(size <=3){
-        return bruteForce(vec_x,size);
-    }
-    int mid = size/2;
-    point mid_p = vec_x[mid];
-    float
-} */
-    point find_closest_pair(std::vector<point> x_vec, std::vector<point> y_vec){
+
+std::pair<point,point> find_closest_pair(std::vector<point> x_vec, std::vector<point> y_vec){
     int m = floor(x_vec.size()/2);
     int l_x=(x_vec[m].x+ x_vec[m+1].x)/2;
-    std::vector<point> x_lvec = x_vec[m];
+    std::vector<point> x_lvec;
     std::vector<point> x_rvec;
-    x_lvec = x_vec.
-} 
-//method to merge left and right
-void merge(std::vector<int> vec, int left, int mid, int right){
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
+    for(int i=0; i<m; i++){
+        x_lvec.push_back(x_vec[i]);
+    }
+    for(int j = m+1; j<x_vec.size();j++){
+        x_rvec.push_back(x_vec[j]);
+    }
+    std::pair<point,point> left_pair = find_closest_pair(x_lvec,y_vec);
+    std::pair<point,point> right_pair = find_closest_pair(x_rvec,y_vec);
+}  
+
+std::pair<point,point> combine(std::vector<point> y_vec, std::vector<point> x_lvec, std::pair<point,point> left_pair,std::pair<point,point> right_pair){
+    float d1 = distance(x_lvec.front(),x_lvec.back());
+}
+
+
+/* void merge(std::vector<point> left_vec, std::vector<point> right_vec){
+    std::vector<point> new_vec;
+    while(!left_vec.empty() && !right_vec.empty()){
+        if(left_vec.begin()<=right_vec.begin()){
+            
+        }
+    }
+}
+void merge(std::vector<int> vec, int p, int r, int q){
+    int n1 = p - q + 2;
+    int n2 = r - q + 1;
     int i,j,k;
 
     std::vector<int> left_vec{n1};
     std::vector<int> right_vec{n2};
 
     for(i = 0; i<n1; i++){
-        left_vec[i]= vec[left+i];
+        left_vec[i]= vec[p+i-1];
     }
+    //left_vec[n1] = infinite;
     for(j = 0; j<n2; j++){
-        right_vec[j]= vec[mid + 1 + j];
+        right_vec[i]= vec[q+j];
     }
-    i = 0;
-    j = 0;
-    k = left;
-    while (i < n1 && j < n2){
-        if(left_vec[i]<=right_vec[j]){
-            vec[k] = left_vec[i];
+    //left_vec[n2] = infinite;
+    i = 1;
+    j = 1;
+    for(k=p;k<=r;k++){
+        if(left_vec[i]<=right_vec){
+            vec[k]=left_vec[i];
             i++;
         }
         else{
-            vec[k] = right_vec[j];
+            vec[k]=right_vec[j];
             j++;
         }
-        k++;  
-    }
-    while (i < n1){
-        vec[k] = left_vec[i];
-        i++;
-        k++;
-    }
-    while (j < n2){
-        vec[k] = right_vec[j];
-        j++;
-        k++;
     }
 }
 //mergesort method
-void mergeSort(std::vector<int> sort, int left, int right){
-    if(left < right){
-        int mid = left + (right - left)/2;
-        mergeSort(sort,left,mid);
-        mergeSort(sort,mid+1, right);
+void mergeSort(std::vector<int> vec, int p, int r){
+    if(p < r){
+        int q = ((p+r)/2);
+        mergeSort(vec,p,q);
+        mergeSort(vec,q+1,r);
 
-        merge(sort,left, mid, right);
+        merge(vec,p, r, q);
     }
     
-}
+} */
 int main(){
+    std::vector<int> test{1,2,7,9,3,4};
     
     return 0;
 }
